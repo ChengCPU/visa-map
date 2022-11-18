16f66149e27c1138c5318be1e59c9d1946dbfb20
ChengCPU <ChengCPU@proton.me>
2022-11-18 19:02:47 +0100

    feature: second draft of table logic

--- pages/table.tsx
+++ pages/table.tsx
@@ -8,7 +8,7 @@ interface Props {
 const countries:string[] = ["abkhazia","afghanistan", "albania","algeria","americanSamoa","andorra","angola","anguilla","antiguaAndBarbuda","argentina","armenia","aruba","australia","austria","azerbaijan","bahamas","bahrain","bangladesh","barbados","belarus","belgium","belize","benin","bermuda","bhutan","bolivia","bonaire","bosniaAndHerzegovina","botswana","bouvetIsland","brazil","britishIndianOceanTerritory","britishVirginIslands","brunei","bulgaria","burkinaFaso","burundi","cambodia","cameroon","canada","capeVerde","caymanIslands","centralAfricanRepublic","chad","chile","china","colombia","comoros","cookIslands","costaRica","croatia","cuba","curacao","cyprus","czechRepublic","democraticRepublicOfTheCongo","denmark","djibouti","dominica","dominicanRepublic","eastTimor","ecuador","egypt","elSalvador","equatorialGuinea","eritrea","estonia","eswatini","ethiopia","falklandIslands","faroeIslands","fiji","finland","france","frenchGuiana","frenchPolynesia","gabon","gambia","georgia","germany","ghana","gibraltar","greece","greenland","grenada","guadeloupe","guam","guatemala","guernsey","guinea","guineaBissau","guyana","haiti","heardIslandAndMcDonaldIslands","honduras","hongKong","hungary","iceland","india","indonesia","iran","iraq","ireland","isleOfMan","israel","italy","ivoryCoast","jamaica","janMayen","japan","jersey","jordan","kazakhstan","kenya","kiribati","kosovo","kuwait","kyrgyzstan","laos","latvia","lebanon","lesotho","liberia","libya","liechtenstein","lithuania","luxembourg","macao","madagascar","malawi","malaysia","maldives","mali","malta","marshallIslands","martinique","mauritania","mauritius","mayotte","mexico","micronesia","moldova","monaco","mongolia","montenegro","montserrat","morocco","mozambique","myanmar","namibia","nauru","nepal","netherlands","newCaledonia","newZealand","nicaragua","niger","nigeria","niue","norfolkIsland","northernCyprus","northernMarianaIslands","northKorea","northMacedonia","norway","oman","pakistan","palau","palestine","panama","papuaNewGuinea","paraguay","peru","philippines","pitcairnIslands","poland","portugal","qatar","republicOfTheCongo","reunion","romania","russia","rwanda","saba","saintBarthelemy","saintHelena","saintKittsAndNevis","saintLucia","saintMartin","saintPierreAndMiquelon","saintVincentAndTheGrenadines","samoa","sanMarino","saoTomeAndPrincipe","saudiArabia","senegal","serbia","seychelles","sierraLeone","singapore","sintEustatius","sintMaarten","slovakia","slovenia","solomonIslands","somalia","southAfrica","southGeorgiaAndTheSouthSandwichIslands","southKorea","southOssetia","southSudan","spain","sriLanka","sudan","suriname","svalbard","sweden","switzerland","syria","taiwan","tajikistan","tanzania","thailand","togo","tokelau","tonga","transnistria","trinidadAndTobago","tunisia","turkey","turkmenistan","turksAndCaicos","tuvalu","uganda","ukraine","unitedArabEmirates","unitedKingdom","unitedStates","unitedStatesVirginIslands","uruguay","uzbekistan","vanuatu","vaticanCity","venezuela","vietnam","wallisAndFutuna","westernSahara","yemen","zambia","zimbabwe"]
 const flags:string[] = ["", "🇦🇫", "🇦🇱", "🇩🇿", "🇦🇸", "🇦🇩", "🇦🇴", "🇦🇮", "🇦🇬", "🇦🇷", "🇦🇲", "🇦🇼", "🇦🇺", "🇦🇹",  "🇦🇿",  "🇧🇸",  "🇧🇭"]
 const main:number[] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-const sub:number[] = [1,2,3,4,5,6,7,8,9,10]
+const sub:number[] = [0,1,2,3,4,5,6,7,8,9]
 const colors:string[] = [
   "abkhaziaColor",
   "afghanistanColor", 
@@ -264,11 +264,11 @@ const colors:string[] = [
 
 const Table:React.FC<Props> = ({ selectArray, assignedColors }) => {
 
-  const renderData = () => {
-    for(let a = 0; a < assignedColors.length; a++) {
-      switch(assignedColors[0][colors[a]]) {
-        case 7: return "visa-free"
-      }
+  const calcVisaRequirements = (sub) => {
+    switch(assignedColors[sub][colors[0]]) {
+      case 7: return "visa-free"
+      case 14: return "visa required"
+      case 12: return "simplified visa"
     }
   }
 
@@ -276,7 +276,7 @@ const Table:React.FC<Props> = ({ selectArray, assignedColors }) => {
     return main.map(main => 
     <tr className={styles.subRow}>
       <Country country={countries[main]} flag={flags[main]} />
-      {sub.map(sub => <td key={Math.random()} className={styles.text}>{renderData()}</td>)}
+      {sub.map(sub => <td className={styles.text}>{calcVisaRequirements(sub)}</td>)}
     </tr>)
   }
 
