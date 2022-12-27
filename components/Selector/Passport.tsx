d5bd61ddb1b1d04a5f7db16d1646670f7c96b917
ChengCPU <ChengCPU@proton.me>
2022-12-27 04:24:26 +0100

    feature: option to render default image if given null as prop

--- components/Selector/Passport.tsx
+++ components/Selector/Passport.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
+import defaultImage from '/public/defaultImage.png'
 interface Props {image:any};
 
 const Passport:React.FC<Props> = ({ image }) => {
   return (
     <Image
       alt={'passport'}
-      src={image}
+      src={(image == null) ? defaultImage : image}
       width={64}
       height={90.8}
     />
