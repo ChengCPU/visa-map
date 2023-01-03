d88023240b8c013f09e17c5425fc2b9f2d077b0e
ChengCPU <ChengCPU@proton.me>
2023-01-03 23:33:34 +0100

    feature: changed to sort by total number of countries with access non-visa required

--- logic/insertionSort.ts
+++ logic/insertionSort.ts
@@ -2,15 +2,15 @@ export default function insertionSort(rankData:any, setRankRender:Function) {
   const unsortedData = rankData
 
   for (let i = 0; i < unsortedData.length; i++) {
-  let current = unsortedData[i][1];
+  let current = unsortedData[i][6];
   let current2 = unsortedData[i];
   let j;
-    for(j=i-1; j >= 0 && unsortedData[j][1] > current;j--) {
+    for(j=i-1; j >= 0 && unsortedData[j][6] > current;j--) {
       unsortedData[j + 1] = unsortedData[j]
-      unsortedData[j + 1][1] = unsortedData[j][1]
+      unsortedData[j + 1][6] = unsortedData[j][6]
     }
   unsortedData[j + 1] = current2;
-  unsortedData[j + 1][1] = current;
+  unsortedData[j + 1][6] = current;
   }
   const sortedData = unsortedData.reverse()
   setRankRender(sortedData)
