5233241770d2870b96def3316d3b43268063a017
ChengCPU <ChengCPU@proton.me>
2023-03-15 13:52:04 +0100

    fix: passport component onHover text changed to include spaces

--- components/Selector/SelectorPassport.tsx
+++ components/Selector/SelectorPassport.tsx
@@ -11,12 +11,13 @@ interface Props {
   setToggle:Function;
   image:object;
   name:string;
+  passportName:string;
 }
 
-const SelectorPassport:React.FC<Props> = ({ setOpenDrawer, select, setSelect, toggle, setToggle, image, name }) => {
+const SelectorPassport:React.FC<Props> = ({ setOpenDrawer, select, setSelect, toggle, setToggle, image, name, passportName }) => {
   return (
     <Button title={name.charAt(0).toUpperCase() + name.slice(1)} onClick={() => {
-      setSelect({ selection: select.selection, passport: name });
+      setSelect({ selection: select.selection, passport: passportName });
       setToggle(!toggle)
       setOpenDrawer(false)
     }}>
