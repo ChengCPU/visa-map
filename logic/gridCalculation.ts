4f75011da48ea43c06d4267091c0501730e7b776
ChengCPU <ChengCPU@proton.me>
2023-03-30 21:20:40 +0200

    fix: changed double quotes to single quotes

--- logic/gridCalculation.ts
+++ logic/gridCalculation.ts
@@ -1,16 +1,16 @@
 export default function gridCalculation(color:string) {
   switch(color) {
-    case "rgb(255,20,147)": return "./homeCountry.png"
-    case "rgb(255,145,0)": return "./gridoecsfom.png"
-    case "rgb(0,135,93)": return "./gridmercosurfom.png"
-    case "rgb(0,51,153)": return "./grideufom.png"
-    case "rgb(153,123,61)": return "./gridgccfom.png"
-    case "rgb(255,179,191)": return "./gridfom.png"
-    case "rgb(50,205,50)": return "./gridvf.png"
-    case "rgb(161,224,123)": return "./gridvoaevisa.png"
-    case "rgb(255,255,92)": return "./gridvoa.png"
-    case "rgb(135,206,250)": return "./gridevisa.png"
-    case "rgb(118,65,171)": return "./gridsp.png"
-    case "rgb(200,200,200)": return "./gridsv.png"
+    case 'rgb(255,20,147)': return './homeCountry.png'
+    case 'rgb(255,145,0)': return './gridoecsfom.png'
+    case 'rgb(0,135,93)': return './gridmercosurfom.png'
+    case 'rgb(0,51,153)': return './grideufom.png'
+    case 'rgb(153,123,61)': return './gridgccfom.png'
+    case 'rgb(255,179,191)': return './gridfom.png'
+    case 'rgb(50,205,50)': return './gridvf.png'
+    case 'rgb(161,224,123)': return './gridvoaevisa.png'
+    case 'rgb(255,255,92)': return './gridvoa.png'
+    case 'rgb(135,206,250)': return './gridevisa.png'
+    case 'rgb(118,65,171)': return './gridsp.png'
+    case 'rgb(200,200,200)': return './gridsv.png'
   }
 }
