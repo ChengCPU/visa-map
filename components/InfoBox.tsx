efbf6cfcc1c36d46be70f9033269c287116375c7
ChengCPU <ChengCPU@proton.me>
2023-04-03 13:49:07 +0200

    feature: infobox displays visa information of selected country

--- components/InfoBox.tsx
+++ components/InfoBox.tsx
@@ -1,9 +1,42 @@
 interface Props {
   mousePos:number | boolean[];
   hover:boolean;
+  countrySelect:string;
+  priority:any;
 }
 
-const InfoBox:React.FC<Props> = ({ mousePos, hover }) => {
+const InfoBox:React.FC<Props> = ({ mousePos, hover, countrySelect, priority }) => {
+
+  const rgbToText = (rgb:string) => {
+    switch(rgb) {
+      case 'rgb(255,20,147)':
+        return 'Home country'
+      case 'rgb(255,145,0)':
+        return 'OECS freedom of movement'
+      case 'rgb(0,135,93)':
+        return 'MERCSOUR freedom of movement'
+      case 'rgb(0,51,153)':
+        return 'EU freedom of movement'
+      case 'rgb(153,123,61)':
+        return 'GCC freedom of movement'
+      case 'rgb(255,179,191)':
+        return 'Freedom of movement'
+      case 'rgb(50,205,50)':
+        return 'Visa-free'
+      case 'rgb(161,224,123)':
+        return 'Visa on arrival/E-visa'
+      case 'rgb(255,255,92)':
+        return 'Visa on arrival'
+      case 'rgb(135,206,250)':
+        return 'E-visa'
+      case 'rgb(118,65,171)':
+        return 'Special permit/police check'
+      case 'rgb(200,200,200)':
+        return 'Simplified visa'
+      case 'rgb(149,150,150)':
+        return 'Visa required'
+    }
+  }
 
   return (
     (hover == true) ?
@@ -27,7 +60,7 @@ const InfoBox:React.FC<Props> = ({ mousePos, hover }) => {
       <table>
         <tbody>
           <tr>
-            <td><p>X: {mousePos[0]} Y: {mousePos[1]}</p></td>
+            <td><p>{rgbToText(priority[countrySelect + 'Color'])}</p></td>
           </tr>
         </tbody>
       </table>
