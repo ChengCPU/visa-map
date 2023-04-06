e5c606594ff2e92ce0340c134d781497210aa2a1
ChengCPU <ChengCPU@proton.me>
2023-04-06 15:23:50 +0200

    fix: changed double quotes to single quotes

--- components/Map/Legend/Rectangle.tsx
+++ components/Map/Legend/Rectangle.tsx
@@ -2,8 +2,8 @@ interface Props {color:string}
 
 const Rectangle:React.FC<Props> = ({ color }) => {
 	return (
-    <svg width="20" height="50">
-    <rect x="2" y="17" width="15" height="15" className={'rectangle'}>
+    <svg width='20' height='50'>
+    <rect x='2' y='17' width='15' height='15' className={'rectangle'}>
       <style jsx>{`
       .rectangle {
       fill: ${color};
