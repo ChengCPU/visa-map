cd72757f7a402ef20aed79706b1975e57933a705
ChengCPU <ChengCPU@proton.me>
2023-04-12 22:48:46 +0200

    fix: removed legend component

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,7 +1,6 @@
 import { useContext } from 'react';
 import { WidthContext } from '../../logic/context/WidthContext';
 import MapSVG from './MapSVG';
-import Legend from './Legend/Legend';
 import styles from '../../styles/Map.module.css';
 type LegendType = {HC:boolean, FoM:boolean, OECSFoM:boolean, MFoM:boolean, EUFoM:boolean, GCCFoM:boolean, VF:boolean, VoAEV:boolean, VoA:boolean, EV:boolean, SP:boolean, CR:boolean}
 interface Props {legend:LegendType; setHover:Function;}
