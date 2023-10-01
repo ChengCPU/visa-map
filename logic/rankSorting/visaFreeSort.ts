2cc663baabb3e801b81f0e9c8d054db5a13fda07
ChengCPU <ChengCPU@proton.me>
2023-10-01 12:41:22 +0200

    feature: rank sorting based on freedom of movement added to visa-free sort page

--- logic/rankSorting/visaFreeSort.ts
+++ logic/rankSorting/visaFreeSort.ts
@@ -1,20 +1,22 @@
 export default function visaFreeSort(rankData:any, setRankRender:Function, sort:string) {
   const unsortedData = rankData
-  let tempDataVf
+  let tempDataVf:any
+  let tempDataFom:any
 
   for (let i = 0; i < unsortedData.length; i++) {
-  let current = unsortedData[i][1];
-  let current2 = unsortedData[i];
-  let j;
-    for(j=i-1; j >= 0 && unsortedData[j][1] > current;j--) {
-      unsortedData[j + 1] = unsortedData[j]
-      unsortedData[j + 1][1] = unsortedData[j][1]
-    }
-  unsortedData[j + 1] = current2;
-  unsortedData[j + 1][1] = current;
+    let current = unsortedData[i][1];
+    let current2 = unsortedData[i];
+    let j;
+      for(j=i-1; j >= 0 && unsortedData[j][1] > current;j--) {
+        unsortedData[j + 1] = unsortedData[j]
+        unsortedData[j + 1][1] = unsortedData[j][1]
+      }
+    unsortedData[j + 1] = current2;
+    unsortedData[j + 1][1] = current;
   }
 
   const sortedData = unsortedData.reverse()
+
   const visaOnArrivalEvisaCheck = (x:number) => {
     if(sortedData[x]?.[2] < sortedData[x + 1]?.[2] && sortedData[x]?.[1] == sortedData[x + 1]?.[1]) {
       tempDataVf = sortedData[x + 1]
@@ -22,11 +24,27 @@ export default function visaFreeSort(rankData:any, setRankRender:Function, sort:
       sortedData[x] = tempDataVf
     }
   }
+
+  const freedomOfMovementCheck = (x:number) => {
+    if(sortedData[x]?.[7] < sortedData[x + 1]?.[7] && sortedData[x]?.[1] == sortedData[x + 1]?.[1]) {
+      tempDataFom = sortedData[x + 1]
+      sortedData[x + 1] = sortedData[x]
+      sortedData[x] = tempDataFom
+    }
+  }
+
   for(let v = 0; v < 5; v++) {
     for(let t = 0; t < sortedData.length; t++) {
       visaOnArrivalEvisaCheck(t)
     }
   }
+
+  for(let j = 0; j < 5; j++) {
+    for(let b = 0; b < sortedData.length; b++) {
+      freedomOfMovementCheck(b)
+    }
+  }
+
   if(sort == 'Ascending') {
     sortedData.reverse()
   }
