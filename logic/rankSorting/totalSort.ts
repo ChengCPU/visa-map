fef17301f6acb4bae422ab10d07bea611e67edc7
ChengCPU <ChengCPU@proton.me>
2023-10-05 18:46:32 +0200

    fix: refactored and added types to rank logic

--- logic/rankSorting/totalSort.ts
+++ logic/rankSorting/totalSort.ts
@@ -1,29 +1,33 @@
 export default function totalSort(rankData:any, setRankRender:Function, sort:string) {
-  const unsortedData = rankData
-  let tempDataVf:any
-  let tempDataVoa:any
-  let tempDataFom:any
-
+  const unsortedData:(string | number)[][] = rankData
 
   for(let i = 0; i < unsortedData.length; i++) {
-    let current = unsortedData[i][6];
-    let current2 = unsortedData[i];
-    let j;
-      for(j=i-1; j >= 0 && unsortedData[j][6] > current;j--) {
-        unsortedData[j + 1] = unsortedData[j]
-        unsortedData[j + 1][6] = unsortedData[j][6]
-      }
+    let current:any = unsortedData[i][6];
+    let current2:(string | number)[] = unsortedData[i];
+    let j:number;
+    
+    for(j = i - 1; j >= 0 && unsortedData[j][6] > current; j--) {
+      unsortedData[j + 1] = unsortedData[j]
+      unsortedData[j + 1][6] = unsortedData[j][6]
+    }
+
     unsortedData[j + 1] = current2;
     unsortedData[j + 1][6] = current;
   }
   
-  const sortedData = unsortedData.reverse()
+  const sortedData:(string | number)[][] = unsortedData.reverse()
 
+  let tempDataVf:(string | number)[]
+  let tempDataVoa:(string | number)[]
+  let tempDataFom:(string | number)[]
+  
   const lessThanVisaFreeCheck = (x:number) => {
     if(sortedData[x]?.[1] < sortedData[x + 1]?.[1] && sortedData[x]?.[6] == sortedData[x + 1]?.[6]) {
       tempDataVf = sortedData[x + 1]
       sortedData[x + 1] = sortedData[x]
       sortedData[x] = tempDataVf
+
+      lessThanVisaFreeCheck(x - 1)
     }
   }
 
@@ -32,42 +36,40 @@ export default function totalSort(rankData:any, setRankRender:Function, sort:str
       tempDataVoa = sortedData[x + 1]
       sortedData[x + 1] = sortedData[x]
       sortedData[x] = tempDataVoa
+
+      visaOnArrivalEvisaCheck(x - 1)
     }
   }
 
   const freedomOfMovementCheck = (x:number) => {
-    if(sortedData[x]?.[7] < sortedData[x + 1]?.[7] && sortedData[x]?.[1] == sortedData[x + 1]?.[1] && sortedData[x]?.[7] < sortedData[x + 1]?.[7] && sortedData[x]?.[6] == sortedData[x + 1]?.[6]) {
+    if(sortedData[x]?.[7] < sortedData[x + 1]?.[7] && sortedData[x]?.[1] == sortedData[x + 1]?.[1] && sortedData[x]?.[2] == sortedData[x + 1]?.[2] && sortedData[x]?.[3] == sortedData[x + 1]?.[3] && sortedData[x]?.[4] == sortedData[x + 1]?.[4] && sortedData[x]?.[5] == sortedData[x + 1]?.[5] && sortedData[x]?.[6] == sortedData[x + 1]?.[6]) {
       tempDataFom = sortedData[x + 1]
       sortedData[x + 1] = sortedData[x]
       sortedData[x] = tempDataFom
+
+      freedomOfMovementCheck(x - 1)
     }
   }
 
-  for(let v = 0; v < 5; v++) {
-    for(let z = 0; z < sortedData.length; z++) {
-      lessThanVisaFreeCheck(z)
-    }
-    for(let t = 0; t < sortedData.length; t++) {
-      visaOnArrivalEvisaCheck(t)
-    }
+  for(let z = 0; z < sortedData.length; z++) {
+    lessThanVisaFreeCheck(z)
+  }
+  
+  for(let t = 0; t < sortedData.length; t++) {
+    visaOnArrivalEvisaCheck(t)
   }
 
-  for(let j = 0; j < 5; j++) {
-    for(let b = 0; b < sortedData.length; b++) {
-      freedomOfMovementCheck(b)
-    }
+  for(let b = 0; b < sortedData.length; b++) {
+    freedomOfMovementCheck(b)
   }
 
   if(sort == 'Ascending') {
     sortedData.reverse()
   }
 
-  const tempArray = []
-  for(let i = 1; i < rankData.length + 1; i++) {
-    tempArray.push(i)
-  }
+  const tempArray = Array.from({ length: rankData.length }, (_, index) => index + 1);
 
-  let count = 0;
+  let count:number = 0
   for(let i = 0; i < rankData.length - 1; i++) {
     tempArray[i] = tempArray[i] - count
     if(rankData[i][1] == rankData[i + 1][1] && rankData[i][2] == rankData[i + 1][2] && rankData[i][3] == rankData[i + 1][3] && rankData[i][4] == rankData[i + 1][4] && rankData[i][5] == rankData[i + 1][5] && rankData[i][6] == rankData[i + 1][6]) {
@@ -75,7 +77,7 @@ export default function totalSort(rankData:any, setRankRender:Function, sort:str
     }
   }
 
-  let x = rankData.length - 2
+  let x:number = rankData.length - 2
   if(rankData[x][1] == rankData[x + 1][1] && rankData[x][2] == rankData[x + 1][2] && rankData[x][3] == rankData[x + 1][3] && rankData[x][4] == rankData[x + 1][4] && rankData[x][5] == rankData[x + 1][5] && rankData[x][6] == rankData[x + 1][6]) {
     tempArray[x + 1] = tempArray[x]
   } else {
