e73f2d6a7191a2e79346fcafc309816a5ea0449b
ChengCPU <ChengCPU@proton.me>
2023-10-23 21:03:15 +0200

    fix: removed unused selectArrayCalculation function

--- logic/selectArrayCalculation.ts
+++
@@ -1,5 +0,0 @@
-interface Select {selection:number;passport:null | string;}
-
-export default function selectArrayCalculation(selectArray:null | string[], setSelectArray:Function, select:Select): void {
-  setSelectArray(selectArray, selectArray[select.selection] = select.passport);
-}
