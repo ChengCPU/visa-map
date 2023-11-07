8112b0fd57dd0525fa431562ab6b291671fc849d
ChengCPU <ChengCPU@proton.me>
2023-11-07 18:29:57 +0100

    fix: rectangle component spacing

--- components/Map/Legend/Rectangle.tsx
+++ components/Map/Legend/Rectangle.tsx
@@ -1,9 +1,9 @@
-interface Props {color:string}
+interface Props {color:string;}
 
 const Rectangle:React.FC<Props> = ({ color }) => {
 	return (
-    <svg width='20' height='50'>
-    <rect x='2' y='17' width='15' height='15' className={'rectangle'}>
+    <svg width='20' height='20'>
+    <rect x='2' y='2' width='15' height='15' className={'rectangle'}>
       <style jsx>{`
       .rectangle {
       fill: ${color};
