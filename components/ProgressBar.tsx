229c69e99bf6ee0650b7b0f21242e6c7601654ce
ChengCPU <ChengCPU@proton.me>
2023-11-19 15:21:19 +0100

    fix: removed comments

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -21,38 +21,24 @@ const ProgressBar:React.FC<Props> = ({ verticalColumn, width, height, color, per
       value={valueCalculation()}
       text={`${valueCalculation()}%`}
       styles={{
-      // Customize the root svg element
       root: {},
-      // Customize the path, i.e. the "completed progress"
       path: {
-        // Path color
         stroke: `${color}`,
-        // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
         strokeLinecap: 'butt',
-        // Customize transition animation
         transition: 'stroke-dashoffset 0.5s ease 0s',
-        // Rotate the path
         transform: 'rotate(0.25turn)',
         transformOrigin: 'center center',
       },
-      // Customize the circle behind the path, i.e. the "total progress"
       trail: {
-        // Trail color
         stroke: '#222222',
-        // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
         strokeLinecap: 'butt',
-        // Rotate the trail
         transform: 'rotate(0.25turn)',
         transformOrigin: 'center center',
       },
-      // Customize the text
       text: {
-        // Text color
         fill: '#ffffff',
-        // Text size
         fontSize: '20px',
       },
-      // Customize background - only used when the `background` prop is true
       background: {
         fill: '#3e98c7',
       },
