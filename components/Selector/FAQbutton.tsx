74a4a40bc9ff249e503591d2995ac5cb742d44da
ChengCPU <ChengCPU@proton.me>
2024-03-08 13:23:04 +0100

    fix: removed props

--- components/Selector/FAQbutton.tsx
+++ components/Selector/FAQbutton.tsx
@@ -1,15 +1,13 @@
-import Image from 'next/image'
 import { Button } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import Link from 'next/link'
-interface Props {};
 
 const CustomizedButton = styled(Button)`
   background-color: #32cd32;
   color: #FFFFFF;
 `
 
-const FAQbutton:React.FC<Props> = ({ }) => {
+const FAQbutton:React.FC = () => {
   return (
     <Link href='/faq'><CustomizedButton variant="contained" size="small" style={{maxWidth: '25px', maxHeight: '25px', minWidth: '25px', minHeight: '25px'}}>{'?'}</CustomizedButton></Link>
   )
