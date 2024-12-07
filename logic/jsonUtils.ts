7265f78f6ecb8867634954749bd026c41e218b44
ChengCPU <ChengCPU@proton.me>
2024-12-07 22:11:43 +0100

    fix: json import function

--- logic/jsonUtils.ts
+++ logic/jsonUtils.ts
@@ -1,29 +1,17 @@
-let cachedJsonData:any = null
-function getBaseUrl() {
-  if(typeof window !== 'undefined') return ''
-  const vc = process.env.VERCEL_URL
-  if(vc) return `https://${vc}`
-  return 'http://localhost:3000/'
+export const getVisaPolicyData = () => {
+  let cachedVisaPolicy
+  if(!cachedVisaPolicy) {
+    cachedVisaPolicy = require('/public/visaPolicy.json')
+  }
+
+  return cachedVisaPolicy
 }
 
-export const fetchAndCacheJsonData = async():Promise<any> => {
-  if(cachedJsonData) {
-    return cachedJsonData
+export const getVisaDurationData = () => {
+  let cachedVisaDuration
+  if(!cachedVisaDuration) {
+    cachedVisaDuration = require('/public/visaDuration.json')
   }
 
-  try {
-    const response = await fetch(getBaseUrl() + 'visaPolicy.json')
-    if(!response.ok) {
-      throw new Error('Network response was not ok.')
-    }
-
-    const data = await response.json()
-    cachedJsonData = data
-
-    return data
-  } catch (error) {
-    console.error('Error fetching JSON data:', error)
-
-    return null
-  }
-};
+  return cachedVisaDuration
+}
--- logic/visaPolicyCalculation/assignedVisaDurationCalculation.ts
+++ logic/visaPolicyCalculation/assignedVisaDurationCalculation.ts
@@ -1,6 +1,6 @@
-import { fetchAndCacheJsonData } from '../visaDurationFetch'
+import { getVisaDurationData } from '../jsonUtils'
 import { MutableRefObject } from 'react'
-const data = await fetchAndCacheJsonData()
+const data = getVisaDurationData()
 
 export default function assignedVisaDurationCalculation(selectRef:MutableRefObject<{selection:number;passport:null|string}>, assignedVisaDurationRef:MutableRefObject<{[key:string]:number}[]>) {
   assignedVisaDurationRef.current[selectRef.current.selection] = data[selectRef.current.passport]
