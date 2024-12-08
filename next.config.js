34e03ce0e081d7086d1abc98e1c109be7cc5a531
ChengCPU <ChengCPU@proton.me>
2024-12-08 18:43:21 +0100

    fix: removed swcMinify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true }
     return config
