758096c06400f15928ed4b019cb8b46f54c67402
ChengCPU <ChengCPU@proton.me>
2025-01-10 19:03:41 +0100

    feature: added ETA-IL

--- components/Table/VisaPolicy.tsx
+++ components/Table/VisaPolicy.tsx
@@ -65,7 +65,7 @@ const VisaPolicy:React.FC<Props> = ({ assignedColorsRef, selectArrayRef, vertica
   }, [])
 
   const ETAcodes:{[key:string]:string | Function} = useMemo(() => {
-    return {CA:'Electronic Travel Authorization',GB:'Electronic Travel Authorization',NZ:'NZeTA',AU:auETAcalc(),US:'ESTA',VI:'ESTA',GU:guamCNMIETAcalc(),MP:guamCNMIETAcalc(),AS:'EPWP',KR:'K-ETA',HK:'Pre-arrival Registration',SC:'SEBS',KE:'Electronic Travel Authorization',PK:'Electronic Travel Authorization',CV:'EASE',MA:'AEVM',SA:'Electronic Visa Waiver',MX:'Electronic Authorization System',MY:'Digital Arrival Card',KH:'e-Arrival Card',BM:'Electronic Travel Authorization',PH:'Electronic Travel Authorization',JJ:'K-ETA',LK:'Electronic Travel Authorization'}
+    return {CA:'Electronic Travel Authorization',GB:'Electronic Travel Authorization',NZ:'NZeTA',AU:auETAcalc(),US:'ESTA',VI:'ESTA',GU:guamCNMIETAcalc(),MP:guamCNMIETAcalc(),AS:'EPWP',KR:'K-ETA',HK:'Pre-arrival Registration',SC:'SEBS',KE:'Electronic Travel Authorization',PK:'Electronic Travel Authorization',CV:'EASE',MA:'AEVM',SA:'Electronic Visa Waiver',MX:'Electronic Authorization System',MY:'Digital Arrival Card',KH:'e-Arrival Card',BM:'Electronic Travel Authorization',PH:'Electronic Travel Authorization',JJ:'K-ETA',LK:'Electronic Travel Authorization',IL:'ETA-IL'}
   }, [])
 
   const visaPolicyCalculation:Function = useCallback(() => {
